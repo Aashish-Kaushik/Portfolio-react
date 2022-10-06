4b49a69f5c07254d9e7b45e8c7ddba6e09b61a2d
Aashish Kaushik <70471873+Aashish-Kaushik@users.noreply.github.com>
2022-10-06 16:14:25 +0530

    upload file local machine to  git repo

---
+++ src/data.js
@@ -0,0 +1,70 @@
+export const projects = [
+  {
+    title: "Node Express  MongoDB JavaScript JWT Authentication",
+    subtitle: "library_manager",
+    description:
+      "Create a book manager app which stores Books and Users data in the DB and create apis to perform crud operation on both of them also  add authentication and authorisation based on JWT tokens on certain actions perfomed on the books by distingusing the role of the user {admin, author, user} and restrict certain apis to user, author and admin respectively.",
+    // image: "./project-1.gif",
+    link: "https://github.com/Aashish-Kaushik/library_manager",
+  },
+  {
+    title: "Node Express  MongoDB JavaScript Passport.js",
+    subtitle: "Project_manager",
+    description:
+      "i build this project to create and manage projects and users. We will have two types of Users. Admin can create projects, see the list of all projects, see the list of all users, Grant or deny a project’s request from a client. Second Client can  Request access from the Admin for a project Can make multiple requests Can see the project they have access to",
+    // image: "./project-1.gif",
+    link: "https://github.com/Aashish-Kaushik/Project_manager",
+  },
+  {
+    title: "JavaScript Node.js EJS and MongoDb ",
+    subtitle: "URl Shortener",
+    description: "I build this for convert a long url into short length ",
+    // image: "./project-1.gif",
+    link: "https://github.com/Aashish-Kaushik/URL-Shortener",
+  },
+  {
+    title: "JavaScript",
+    subtitle: "Simon Game",
+    description:
+      "simon game in this it will generate Diifrent No. every time and click with squence. if you forget squence then game will be over",
+    // image: "./project-1.gif",
+    link: "https://simon009.netlify.app/",
+  },
+  {
+    title: "JavaScript",
+    subtitle: "CLI QUIZ",
+    description:
+      "This is a basic Quiz based on the End Game. So test how much you remember it",
+    // image: "./project-2.gif",
+    link: "https://replit.com/@AashishKaushik1/end-game?embed=1&output=1#index.js/",
+  },
+  {
+    title: "Html CSS & JavaScript",
+    subtitle: "Groot-translator",
+    description:
+      "This is a Groot translator which convert your language into Groot language it",
+    // image: "./project-3.gif",
+    link: "https://groottranslater.netlify.app/",
+  },
+  {
+    title: "Html CSS &  JavaScript",
+    subtitle: "Dice-Game",
+    description:
+      "Dice game in this it will generate dice no. and declare the winner",
+    // image: "./project-4.gif",
+    link: "https://dicegametest.netlify.app/",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Express",
+  "Node",
+  "MongoDB",
+  "jQuery",
+  "EJS",
+  "Git & GitHub",
+  "HTML",
+  "CSS",
+];
