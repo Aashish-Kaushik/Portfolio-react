aa64ba313059ae6ac5cfd8190b2eca9c82bc4eed
Aashish Kaushik <70471873+Aashish-Kaushik@users.noreply.github.com>
2023-06-22 16:28:52 +0530

    change text of projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,9 +12,7 @@ export default function Projects() {
             Projects I've Built
           </h1>
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo
-            facilis repellat ab cupiditate alias vero aliquid obcaecati quisquam
-            fuga dolore.
+          This collection serves as a testament to my passion for innovation, problem-solving, and my ability to deliver exceptional results.
           </p>
         </div>
         <div className="flex flex-wrap -m-4">
