00ffa67f512e2d7f7ed76b04a2f907a2a486e308
Aashish Kaushik <70471873+Aashish-Kaushik@users.noreply.github.com>
2025-02-05 14:05:51 +0530

    change skill array

--- src/data.js
+++ src/data.js
@@ -1,97 +1,96 @@
 export const projects = [
   {
-    title: "Chat APP",
-    subtitle: "Real-Time Chat Application",
-    description: "This is a real-time chat application built using Node.js, Express, and Socket.IO. It allows users to engage in instant messaging and communicate with each other in real-time Users can send and receive messages instantly without the need to refresh the page. The chat app supports multiple users simultaneously. The chat interface is intuitive and easy to use. Create room for grop chat",
-    link:"https://github.com/Aashish-Kaushik/ChatApp"
-  }, 
-  {
-    title:"Store-API",
-    subtitle:"Store API Project",
-    description:"This repository contains the code for the Store API project. The project aims to provide a RESTful API for managing a store's inventory and sales data. It allows users to perform various operations such as retrieving sales reports.",
-    link:"https://github.com/Aashish-Kaushik/Store-API"
-  },
-  {
-    title:"ToDo App",
-    subtitle:"Todo API Project",
-    description:"This repository contains the code for the Todo API project. The project aims to provide a RESTful API for managing todo items. It allows users to create, update, delete, and retrieve todo items.",
-    link:"https://github.com/Aashish-Kaushik/Todo-app"
-  },
-  {
-    title: "Node",
-    subtitle: "Slots-tracker-CLI app ",
+    title: 'Chat APP',
+    subtitle: 'Real-Time Chat Application',
     description:
-      "I build a COVID-19 vaccination slots tracker using CLI. This will call the live api and return the different slots available in your choice of district..",
-    // image: "./project-1.gif",
-    link: "https://github.com/Aashish-Kaushik/Slots-tracker-CLI",
+      'This is a real-time chat application built using Node.js, Express, and Socket.IO. It allows users to engage in instant messaging and communicate with each other in real-time Users can send and receive messages instantly without the need to refresh the page. The chat app supports multiple users simultaneously. The chat interface is intuitive and easy to use. Create room for grop chat',
+    link: 'https://github.com/Aashish-Kaushik/ChatApp',
   },
   {
-    title: "Node Express  MongoDB JavaScript JWT Authentication",
-    subtitle: "library_manager",
+    title: 'Store-API',
+    subtitle: 'Store API Project',
     description:
-      "Create a book manager app which stores Books and Users data in the DB and create apis to perform crud operation on both of them also  add authentication and authorisation based on JWT tokens on certain actions perfomed on the books by distingusing the role of the user {admin, author, user} and restrict certain apis to user, author and admin respectively.",
-    // image: "./project-1.gif",
-    link: "https://github.com/Aashish-Kaushik/library_manager",
+      "This repository contains the code for the Store API project. The project aims to provide a RESTful API for managing a store's inventory and sales data. It allows users to perform various operations such as retrieving sales reports.",
+    link: 'https://github.com/Aashish-Kaushik/Store-API',
   },
   {
-    title: "Node Express  MongoDB JavaScript Passport.js",
-    subtitle: "Project_manager",
+    title: 'ToDo App',
+    subtitle: 'Todo API Project',
     description:
-      "i build this project to create and manage projects and users. We will have two types of Users. Admin can create projects, see the list of all projects, see the list of all users, Grant or deny a project’s request from a client. Second Client can  Request access from the Admin for a project Can make multiple requests Can see the project they have access to",
-    // image: "./project-1.gif",
-    link: "https://github.com/Aashish-Kaushik/Project_manager",
+      'This repository contains the code for the Todo API project. The project aims to provide a RESTful API for managing todo items. It allows users to create, update, delete, and retrieve todo items.',
+    link: 'https://github.com/Aashish-Kaushik/Todo-app',
   },
   {
-    title: "JavaScript Node.js EJS and MongoDb ",
-    subtitle: "URl Shortener",
-    description: "I build this for convert a long url into short length ",
-    // image: "./project-1.gif",
-    link: "https://github.com/Aashish-Kaushik/URL-Shortener",
-  },
-  {
-    title: "JavaScript",
-    subtitle: "Simon Game",
+    title: 'Node',
+    subtitle: 'Slots-tracker-CLI app ',
     description:
-      "simon game in this it will generate Diifrent No. every time and click with squence. if you forget squence then game will be over",
+      'I build a COVID-19 vaccination slots tracker using CLI. This will call the live api and return the different slots available in your choice of district..',
     // image: "./project-1.gif",
-    link: "https://simon009.netlify.app/",
+    link: 'https://github.com/Aashish-Kaushik/Slots-tracker-CLI',
   },
   {
-    title: "JavaScript",
-    subtitle: "CLI QUIZ",
+    title: 'Node Express  MongoDB JavaScript JWT Authentication',
+    subtitle: 'library_manager',
     description:
-      "This is a basic Quiz based on the End Game. So test how much you remember it",
+      'Create a book manager app which stores Books and Users data in the DB and create apis to perform crud operation on both of them also  add authentication and authorisation based on JWT tokens on certain actions perfomed on the books by distingusing the role of the user {admin, author, user} and restrict certain apis to user, author and admin respectively.',
+    // image: "./project-1.gif",
+    link: 'https://github.com/Aashish-Kaushik/library_manager',
+  },
+  {
+    title: 'Node Express  MongoDB JavaScript Passport.js',
+    subtitle: 'Project_manager',
+    description:
+      'i build this project to create and manage projects and users. We will have two types of Users. Admin can create projects, see the list of all projects, see the list of all users, Grant or deny a project’s request from a client. Second Client can  Request access from the Admin for a project Can make multiple requests Can see the project they have access to',
+    // image: "./project-1.gif",
+    link: 'https://github.com/Aashish-Kaushik/Project_manager',
+  },
+  {
+    title: 'JavaScript Node.js EJS and MongoDb ',
+    subtitle: 'URl Shortener',
+    description: 'I build this for convert a long url into short length ',
+    // image: "./project-1.gif",
+    link: 'https://github.com/Aashish-Kaushik/URL-Shortener',
+  },
+  {
+    title: 'JavaScript',
+    subtitle: 'Simon Game',
+    description:
+      'simon game in this it will generate Diifrent No. every time and click with squence. if you forget squence then game will be over',
+    // image: "./project-1.gif",
+    link: 'https://simon009.netlify.app/',
+  },
+  {
+    title: 'JavaScript',
+    subtitle: 'CLI QUIZ',
+    description: 'This is a basic Quiz based on the End Game. So test how much you remember it',
     // image: "./project-2.gif",
-    link: "https://replit.com/@AashishKaushik1/end-game?embed=1&output=1#index.js/",
+    link: 'https://replit.com/@AashishKaushik1/end-game?embed=1&output=1#index.js/',
   },
   {
-    title: "Html CSS & JavaScript",
-    subtitle: "Groot-translator",
-    description:
-      "This is a Groot translator which convert your language into Groot language it",
+    title: 'Html CSS & JavaScript',
+    subtitle: 'Groot-translator',
+    description: 'This is a Groot translator which convert your language into Groot language it',
     // image: "./project-3.gif",
-    link: "https://groottranslater.netlify.app/",
+    link: 'https://groottranslater.netlify.app/',
   },
   {
-    title: "Html CSS &  JavaScript",
-    subtitle: "Dice-Game",
-    description:
-      "Dice game in this it will generate dice no. and declare the winner",
+    title: 'Html CSS &  JavaScript',
+    subtitle: 'Dice-Game',
+    description: 'Dice game in this it will generate dice no. and declare the winner',
     // image: "./project-4.gif",
-    link: "https://dicegametest.netlify.app/",
+    link: 'https://dicegametest.netlify.app/',
   },
 ];
 
 export const skills = [
-  "JavaScript",
-  "React",
-  "Express",
-  "Node",
-  "MongoDB",
-  "jQuery",
-  "EJS",
-  "Git & GitHub",
-  "MySQL",
-  "HTML",
-  "CSS",
+  'JavaScript',
+  'React',
+  'Express',
+  'Node',
+  'MongoDB',
+  'Git & GitHub',
+  'MySQL',
+  'HTML',
+  'CSS',
+  'Tailwind',
 ];
